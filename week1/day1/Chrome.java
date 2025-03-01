4203b5a4cd09b848bebdee75ff4d132acd49e0c6
Vijayos <58661405+Vijayos@users.noreply.github.com>
2025-03-02 00:33:15 +0530

    Add files via upload
    Week1_Day1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Chrome Version : "+ version);
+		System.out.println("Chrome Developer : "+ developer);
+		System.out.println("Chrome is Beta : "+ isBeta);
+		System.out.println("Chrome Release Year : "+ releaseYear);
+		System.out.println("Chrome ShortCutkey : "+ shortcutKey);
+		
+
+	}
+
+}
---
+++ week1/day2/SecondProgram.java
@@ -0,0 +1,10 @@
+package week1.day2;
+
+public class SecondProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Second program");
+	}
+
+}
